12d7b0f42b54cb1b53984e76dec138beb1f0d758
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2025-07-18 18:29:08 +0500

    Update Fibonacci-Series.cpp

--- Fibonacci-Series.cpp
+++ Fibonacci-Series.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
-
+    int terms;
     cout << "Enter the number of terms: ";
-    cin >> n;
-
-    cout << "Fibonacci Series: ";
-    for (int i = 1; i <= n; ++i) {
-        cout << t1 << ", ";
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
+    cin >> terms;
+    
+    cout << "Fibonacci Series up to " << terms << " terms:\n";
+    for (int i = 0; i < terms; ++i) {
+        cout << fibonacci(i) << " ";
     }
+    
     return 0;
 }
